6c8133cc4c41efd8759dd801f09d40b84d529953
laura-gm-456 <loragarmur@gmail.com>
2024-12-13 15:23:54 +0100

    se agregan los filtros de las categorias del menu

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,22 @@
 //DEBE imprimir en pantalla la información de filtros.
 
-//DEBE imprimir en pantalla los productos, con su Título, descripción y precio en € y botón de añadir.
+
+import {filters} from '../assets/data/data.js'
+
+
+let contenedorFiltros = document.getElementById('filters');
+
+function mostrarFiltros(listaFiltros){
+      contenedorFiltros.innerHTML = '';// Quita el elemto filtro vacio
+      listaFiltros.forEach(filter => { //Sirve para recorrer cada elemento de filters
+          let filtroSection = document.createElement('div');// Crea un boton
+          filtroSection.innerHTML = `
+          <button class="filter">${filter}</button>`; 
+      contenedorFiltros.appendChild(filtroSection);
+      });
+      }
+
+      mostrarFiltros(filters);
+
+      
+//DEBE imprimir en pantalla los productos, con su Título, descripción y precio en € y botón de añadir.git pull origin dev
