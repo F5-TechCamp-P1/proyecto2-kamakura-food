2ac34743df8c88c1c00486bd9fa8deeba1f5da16
unknown <gaviriayeferson2@gmail.com>
2024-12-14 17:36:03 +0100

    Se actualiza rama

--- src/menu.js
+++ src/menu.js
@@ -3,18 +3,30 @@
 
 
 //DEBE imprimir en pantalla los productos, con su Título, descripción y precio en € y botón de añadir.
-import {products} from '../assets/data/data.js'
-import {filters} from '../assets/data/data.js'
-//console.log(products);
+
+import {filters, products} from '../assets/data/data.js'
+
+
+
 
 let contenedorProductos = document.getElementById('products'); //Acceso al contenedor de products del Html
+let contenedorFiltros = document.getElementById('filters');
 
+    function mostrarFiltros(listaFiltros){
+        contenedorFiltros.innerHTML = '';// Quita el elemto filtro vacio
+        listaFiltros.forEach(filter => { //Sirve para recorrer cada elemento de filters
+            let filtroSection = document.createElement('div');// Crea un boton
+            filtroSection.innerHTML = `
+            <button class="filter">${filter}</button>`; 
+        contenedorFiltros.appendChild(filtroSection);
+        });
+        } 
 
-function mostrarProductos(listaProductos) { // Funcion para mostrar productos
-        contenedorProductos.innerHTML = '';
-        listaProductos.forEach(product => { //Sirve para recorrer cada elemento del array
-        let productoDiv = document.createElement('div');// Crea el contenedor de cada prodcuto para que se muestre
-        productoDiv.className = 'product-container';//className asigna el estilo ya asiganado para que se vea como en el video
+    function mostrarProductos(listaProductos) { // Funcion para mostrar productos
+            contenedorProductos.innerHTML = '';// Quita el elemto producto vacio
+                listaProductos.forEach(product => { //Sirve para recorrer cada atributo del producto
+                let productoDiv = document.createElement('div');// Crea el contenedor de cada prodcuto para que se muestre
+                productoDiv.className = 'product-container';//className asigna el estilo ya asiganado para que se vea como en el video
 
         // Este es el contenedor para mostrar los productos de manera dinamica, van en template literal `
         // Define el contenido HTML del div, insertando dinámicamente los datos del producto 
@@ -31,7 +43,7 @@ function mostrarProductos(listaProductos) { // Funcion para mostrar productos
 }
 // Mostrar los productos al cargar la página
 mostrarProductos(products);
-
+mostrarFiltros(filters);
 /*document.addEventListener('DOMContentLoaded', () => {
    // mostrarProductos(products); // Mostrar todos los productos
 });*/
