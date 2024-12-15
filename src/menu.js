c94730dcad1eb7d7a959559239bcd42939089265
Ayuik <marcosayelen@gmail.com>
2024-12-15 11:48:57 +0100

    filtros funcionales

--- src/menu.js
+++ src/menu.js
@@ -21,6 +21,7 @@ function mostrarFiltros(listaFiltros){
 
       mostrarFiltros(filters);
      
+//
 
 let contenedorProductos = document.getElementById('products'); //Acceso al contenedor de products del Html
 
@@ -51,3 +52,35 @@ mostrarProductos(products);
    // mostrarProductos(products); // Mostrar todos los productos
 });*/
 
+let botonesFiltros = document.getElementsByClassName('filter');
+
+function filtrarRamen() { 
+    let productosFiltrados = products.filter(product => product.category === "ramen"); 
+    mostrarProductos(productosFiltrados); 
+};
+
+function filtrarSushi() { 
+    let productosFiltrados = products.filter(product => product.category === "sushi"); 
+    mostrarProductos(productosFiltrados); 
+};
+
+function filtrarEntradas() { 
+    let productosFiltrados = products.filter(product => product.category === "entradas"); 
+    mostrarProductos(productosFiltrados); 
+};
+
+function filtrarPostres() { 
+    let productosFiltrados = products.filter(product => product.category === "postres"); 
+    mostrarProductos(productosFiltrados); 
+};
+
+function filtrarTodos () {
+    let productosFiltrados = products;
+    mostrarProductos(productosFiltrados);
+};
+
+botonesFiltros[0].addEventListener('click', filtrarTodos);
+botonesFiltros[1].addEventListener('click', filtrarRamen);
+botonesFiltros[2].addEventListener('click', filtrarSushi);
+botonesFiltros[3].addEventListener('click', filtrarEntradas);
+botonesFiltros[4].addEventListener('click', filtrarPostres);
