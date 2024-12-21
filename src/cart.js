d0fe7182a98d5fc32fbfba436e16f8d098818739
Ayuik <marcosayelen@gmail.com>
2024-12-21 12:55:42 +0100

    rama actualizada con cambio de dev

--- src/cart.js
+++ src/cart.js
@@ -2,10 +2,15 @@
 import { products } from "../assets/data/data.js";
 import { recogerInfoParaRecibo } from "./receipt.js";
 
-let $botonCarrito = document.getElementById('cart');
 let $contenedorCarrito = document.getElementById('cart-container');
+let precioTotal = 0;
 
-function conmutarCarrito (){
+let precioTotalLabel = document.getElementById("cart-total");
+export function actualizarPrecioTotal(precio) {
+    precioTotalLabel.innerHTML = `Total: ${precio.toFixed(2)} €`;
+}
+
+export function conmutarCarrito (){
       if ($contenedorCarrito.style.display === "none" || $contenedorCarrito.style.display === "" ) {
       $contenedorCarrito.style.display = "block"
      } else {
@@ -13,7 +18,7 @@ function conmutarCarrito (){
      }
 }
 
-$botonCarrito.addEventListener('click', conmutarCarrito);
+//$botonCarrito.addEventListener('click', conmutarCarrito);
 
 // Añade pedidos de productos al carrito de compras.
 
@@ -22,13 +27,15 @@ function insertarProductoEnCarrito(producto, cantidad){
       let $plantillaProductoEnCarrito = document.createElement('div');
       $plantillaProductoEnCarrito.className = 'cart-container';
 
-      $contenedorCarrito.style.display = "block"
+      $contenedorCarrito.style.display = "block";
+      precioTotal = precioTotal + (producto.price);
+      actualizarPrecioTotal(precioTotal);
 
       $plantillaProductoEnCarrito.innerHTML = `
             <button class="close-button"><img src="./assets/img/close.svg" alt="close"></button>
             <div class="text-container" data-id="${producto.id}">
                   <h3>${producto.name}</h3>
-                  <h5 data-price="${producto.price}">${producto.price.toFixed(2)} €</h5>
+                  <h5 data-price="${producto.price}" class="product-price">${producto.price.toFixed(2)} €</h5>
             </div>
             <div class="quantity-container" id="quantity">
                   <button>+</button>
@@ -36,7 +43,9 @@ function insertarProductoEnCarrito(producto, cantidad){
                   <button>-</button>
             </div>`
       $contenedorProductosEnCarrito.append($plantillaProductoEnCarrito);
-      recogerInfoParaRecibo();
+
+      recogerInfoParaRecibo()
+
 }
 
 function buscarProductoPorId(id) {
@@ -74,12 +83,17 @@ function manejadorDeEventoCarritoModificado(evento) {
 document.addEventListener('carritoModificado', manejadorDeEventoCarritoModificado);
 
 // Eliminar Producto añadido del carrito
-
 document.getElementById("cart-products").addEventListener("click", (evento) => {
+
       if (evento.target.closest(".close-button")) {
-          let $productoDiv = evento.target.closest(".cart-container");
-          $productoDiv.remove(); // Elimina solo este producto
-            }
+            let $productoDiv = evento.target.closest(".cart-container");
+            precioTotal = precioTotal -
+            (parseFloat($productoDiv.querySelector(".product-price").dataset.price) *
+            (parseFloat($productoDiv.querySelector(".quantity").textContent)));
+
+            actualizarPrecioTotal(precioTotal);
+            $productoDiv.remove(); // Elimina solo este producto
+      }
   });
 
 // Aumentar el precio mostrado en la ficha del carrito, multiplicando el precio original que viene de data por la cantidad que se muestre en el elemento quantity
@@ -87,30 +101,35 @@ document.getElementById("cart-products").addEventListener("click", (evento) => {
 
 // Evento delegador para los botones "+" y "-"
 document.getElementById("cart-products").addEventListener("click", (evento) => {
-      const $boton = evento.target; 
+      const $boton = evento.target;
       let $productoDiv = $boton.closest(".cart-container");
-      
-      let $cantidad = $productoDiv.querySelector(".quantity"); 
+
+      let $cantidad = $productoDiv.querySelector(".quantity");
       let cantidadActual = parseInt($cantidad.textContent);
       let $precioElemento = $productoDiv.querySelector(".text-container h5");
       let precioOriginal = parseFloat($precioElemento.dataset.price);
-    
- 
+
+
        if ($boton.textContent === "+") {
-          cantidadActual++; 
-      
+             cantidadActual++;
+             precioTotal = precioTotal + precioOriginal;
+             actualizarPrecioTotal(precioTotal);
+
        } else if ($boton.textContent === "-"){
            if (cantidadActual >1){
             cantidadActual--;
-            
+            precioTotal = precioTotal - precioOriginal;
+            actualizarPrecioTotal(precioTotal);
+
            } else if (cantidadActual===1){
             $productoDiv.remove();
-           } 
+            precioTotal = precioTotal - precioOriginal;
+            actualizarPrecioTotal(precioTotal);
+
+           }
 
        }
        $cantidad.textContent = cantidadActual;
        let subtotal = cantidadActual * precioOriginal;
        $precioElemento.textContent = `${subtotal.toFixed(2)} €`;
-      })         
-      
-   
+      })
